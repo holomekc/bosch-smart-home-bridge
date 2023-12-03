1e8acaa9eedc15372c0873aed48d930c55ad34ba
holomekc <30546982+holomekc@users.noreply.github.com>
2023-12-03 13:16:25 +0100

    test

--- src/bshb-utils.spec.ts
+++ src/bshb-utils.spec.ts
@@ -1,11 +1,19 @@
 // src/add.spec.ts
 import { BshbUtils } from "./bshb-utils";
 
-describe("identifier", () => {
-  it("should add two numbers", () => {
+describe("BshbUtils", () => {
+  it("generate identifier", () => {
     const identifier = BshbUtils.generateIdentifier();
 
     expect(identifier).not.toBeNull();
     expect(identifier.length).toBeGreaterThan(0);
+  });
+
+  it("generate client certificate", () => {
+    const result = BshbUtils.generateClientCertificate();
+
+    expect(result).not.toBeNull();
+    expect(result.cert).not.toBeNull();
+    expect(result.private).not.toBeNull();
   });
 });
